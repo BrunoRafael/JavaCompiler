8adcc7bbac183c240e69c7adb094f9918e85131b
julioandherson <julio.silva@ccc.ufcg.edu.br>
2015-06-08 20:31:10 -0300

    Teste para comentario

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,13 +9,14 @@ public class Main {
         try {
             for (int i = 0; i < args.length; i++) {
                 System.out.println("Program " + args[i]);
-                Lexer scanner = new Lexer(
-                        new java.io.FileReader(args[i]));
+                Lexer scanner = new Lexer(new java.io.FileReader(args[i]));
                 ParserCup p = new ParserCup(scanner);
                 p.parse();
+                System.out.println("No errors.");
             }
         } catch (Exception e) {
             System.out.println(e);
+            System.exit(1);
         }
     }
 }
